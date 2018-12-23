edfb70d9ec1fc1046f5fc58ca7bb1f120ab7e31b
willy418785 <spdf7785gh>
2018-12-23 15:48:03 +0800

    Add event listener for monitoring players' mouse input

--- EventLisenterHandler.js
+++ EventLisenterHandler.js
@@ -3,20 +3,41 @@ class EventListenerHandler{
         this.inputDocument = inputDocument;
         this.playerInput = playerInput;
     }
-    AddEventListener(){
+    AddKeypressEventListener(){
         var _this = this;
         this.inputDocument.addEventListener("keypress", function(e){ _this.keyPressHandler(e); }, false); 
     }
+    AddClickEventListener(){
+        var _this = this;
+        this.inputDocument.addEventListener("mousedown", function(e){ _this.clickHandler(e); }, false); 
+    }
+    clickHandler(e){
+        if(e.button === 0){
+            this.playerInput.playerInputList['attack1'] = true;
+        }
+        if(e.button === 2){
+            this.playerInput.playerInputList['attack2'] = true;
+        }
+        //console.log(playerKeyboardInputObj);
+    }
     keyPressHandler(e){
         if (e.keyCode == 68 || e.keyCode == 100) {
+            this.playerInput.playerInputList['right'] = true;
         }
         if (e.keyCode == 65 || e.keyCode == 97) {
+            this.playerInput.playerInputList['left'] = true;
         }
         if (e.keyCode == 87 || e.keyCode == 119) {
+            this.playerInput.playerInputList['up'] = true;
         }
         if (e.keyCode == 83 || e.keyCode == 115) {
+            this.playerInput.playerInputList['down'] = true;
         }
-        this.playerKeyboardInputObj = 1;
-        //console.log(playerKeyboardInputObj);
+        if(e.keyCode == 32){
+            this.playerInput.playerInputList['specialMovement'] = true;
+        }
+        for (var key in this.playerInput.playerInputList) {        
+            console.log(key, this.playerInput.playerInputList[key]);
+        }
     }
 }
