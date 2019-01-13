f3ecb8b6a91c1f5ecc64db685953c9479f83f843
willy418785 <spdf7785gh>
2019-01-13 15:24:04 +0800

    add some new variables corrseponding to speed control to Character

--- Character.js
+++ Character.js
@@ -1,9 +1,27 @@
-class Character{
-    constructor(gravityCoefficient, characterWidth, characterHeight, ATTACK_ANIMATION_FRAME, TURN_AROUND_FRAME, x, y){
-        this.fallSpeed = 0;
-        this.horizontalSpeed = 0;
+class Character {
+    constructor(
+        gravityCoefficient,
+        characterWidth,
+        characterHeight,
+        ATTACK_ANIMATION_FRAME,
+        TURN_AROUND_FRAME,
+        x,
+        y,
+        verticalSpeadUpperBound,
+        verticalSpeadLowerBound,
+        horizontalSpeadUpperBound,
+        horizontalSpeadLowerBound,
+        verticalSpeedAccelerationUpperBound,
+        verticalSpeedAccelerationLowerBound,
+        horizontalSpeedAccelerationUpperBound,
+        horizontalSpeedAccelerationLowerBound) {
+            
+        this.verticalSpeed = 0; // positive direction is down
+        this.verticalSpeedAcceleration = 0;
+        this.horizontalSpeed = 0; // positive direction is right
+        this.horizontalSpeedAcceleration = 0;
         this.turnAroundAnimationCount = 0
-        this.attackAnimationCount = 0; 
+        this.attackAnimationCount = 0;
         this.isTurningRight = true;
         this.isTurningAround = false;
         this.x = x;
@@ -13,6 +31,14 @@ class Character{
         this.characterHeight = characterHeight;
         this.ATTACK_ANIMATION_FRAME = ATTACK_ANIMATION_FRAME;
         this.TURN_AROUND_FRAME = TURN_AROUND_FRAME;
+        this.verticalSpeadUpperBound = verticalSpeadUpperBound;
+        this.verticalSpeadLowerBound = verticalSpeadLowerBound;
+        this.horizontalSpeadUpperBound = horizontalSpeadUpperBound;
+        this.horizontalSpeadLowerBound = horizontalSpeadLowerBound;
+        this.verticalSpeedAccelerationUpperBound = verticalSpeedAccelerationUpperBound;
+        this.verticalSpeedAccelerationLowerBound = verticalSpeedAccelerationLowerBound;
+        this.horizontalSpeedAccelerationUpperBound = horizontalSpeedAccelerationUpperBound;
+        this.horizontalSpeedAccelerationLowerBound = horizontalSpeedAccelerationLowerBound;
     }
 
     // addBuff(){
