3fee79958c6c4adbcd00e6397e5a03d304e20ef2
willy418785 <spdf7785gh>
2019-03-10 16:47:11 +0800

    redesign functionality of PlayerInputProcessor

---
+++ PlayerInputProcessor.js
@@ -0,0 +1,18 @@
+class PlayerInputProcessor{
+    constructor(character, playerInput) {
+        this.character = character;
+        this.playerInput = playerInput;
+    }
+    process(){
+        
+    }
+    characterMove(){
+
+    }
+    characterAttack(){
+
+    }
+    characterAttackWhileMoving(){
+        
+    }
+}
