da7900cfa67ce0d5bc30db88a7559af15c4805fc
willy418785 <spdf7785gh>
2019-04-14 15:37:22 +0800

    implement the basic functionality of characterMoveSideway().

--- PlayerAction.js
+++ PlayerAction.js
@@ -1,22 +1,40 @@
-class PlayerAction{
-    constructor() {
+class PlayerAction {
+    constructor() {}
+    characterMoveSideway(character, isMovingRight) {
+        var accelerationUnit =
+            isMovingRight ?
+            character.rightSpeedAccelerationUnit : character.leftySpeedAccelerationUnit;
+        var speedAccelerationUpperBound =
+            isMovingRight ?
+            character.rightSpeedAccelerationUpperBound : character.leftySpeedAccelerationUpperBound;
+        var isLessThanUpperBound =
+            character.horizontalSpeedAcceleration + accelerationUnit <= speedAccelerationUpperBound;
+
+        var acceleration;
+        if (isLessThanUpperBound) {
+            acceleration = character.horizontalSpeedAcceleration + accelerationUnit;
+        } else {
+            acceleration = character.horizontalSpeedAccelerationUpperBound;
+        }
+        if (!isMovingRight) {
+            acceleration = -acceleration;
+        }
+        character.horizontalSpeed += acceleration;
+        character.x += character.horizontalSpeed;
     }
-    characterMoveSideway(){
-        
-    }
-    characterMoveUp(){
+    characterMoveUp() {
 
     }
-    characterMoveDown(){
+    characterMoveDown() {
 
     }
-    characterAttackSideway(){
+    characterAttackSideway() {
 
     }
-    characterAttackUp(){
+    characterAttackUp() {
 
     }
-    characterAttackDown(){
+    characterAttackDown() {
 
     }
 }
