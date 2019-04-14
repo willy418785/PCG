546c0bdd5b7d3f15882ac8a38c7e8804575b1762
willy418785 <spdf7785gh>
2019-04-14 15:39:06 +0800

    modify PlayerInputProcessor to test the functionality of PlayerAction.characterMoveSideway()

--- PlayerInputProcessor.js
+++ PlayerInputProcessor.js
@@ -1,18 +1,35 @@
-class PlayerInputProcessor{
+class PlayerInputProcessor {
     constructor(character, playerInput) {
         this.character = character;
         this.playerInput = playerInput;
+        this.playerAction = new PlayerAction();
     }
-    process(){
-        
+    process() {
+        if (this.playerInput.isProccessed) {
+            return;
+        }
+
+        if (playerInput.playerInputList['right'] &&
+            playerInput.playerInputList['left']) {
+            return;
+        }
+        if(playerInput.playerInputList['right']){
+            this.characterMove(true);
+            
+        }
+        if(playerInput.playerInputList['left']){
+            this.characterMove(false);
+        }
+        this.playerInput.reset();
+        this.playerInput.isProccessed = true;
     }
-    characterMove(){
+    characterMove(isMovingRight) {
+        this.playerAction.characterMoveSideway(this.character, isMovingRight);
+    }
+    characterAttack() {
 
     }
-    characterAttack(){
+    characterAttackWhileMoving() {
 
-    }
-    characterAttackWhileMoving(){
-        
     }
 }
