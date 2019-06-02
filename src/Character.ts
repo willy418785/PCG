11afc9376214984d76a00e7794278c3d95bdf5ca
willy418785 <spdf7785gh>
2019-06-02 15:17:00 +0800

    redesign method process() to handle value overbound of horizontal speed

--- src/Character.ts
+++ src/Character.ts
@@ -46,38 +46,45 @@ export class Character {
         lowerSpeedAccelerationUpperBound,
         rightSpeedAccelerationUpperBound,
         leftySpeedAccelerationUpperBound) {
-            this.verticalSpeed = 0;
-            this.verticalSpeedAcceleration = 0;
-            this.horizontalSpeed = 0;
-            this.horizontalSpeedAcceleration = 0;
-            this.turnAroundAnimationCount = 0;
-            this.attackAnimationCount = 0;
-            this.isTurningRight = true;
-            this.isTurningAround = false;
-            
-            this.gravityCoefficient = gravityCoefficient;
-            this.characterWidth = characterWidth;
-            this.characterHeight = characterHeight;
-            this.ATTACK_ANIMATION_FRAME = ATTACK_ANIMATION_FRAME;
-            this.TURN_AROUND_FRAME = TURN_AROUND_FRAME;
-            this.x = x;
-            this.y = y;
-            this.upperSpeedAccelerationUnit = upperSpeedAccelerationUnit;
-            this.lowerSpeedAccelerationUnit = lowerSpeedAccelerationUnit;
-            this.rightSpeedAccelerationUnit = rightSpeedAccelerationUnit;
-            this.leftySpeedAccelerationUnit = leftySpeedAccelerationUnit;
-            this.upperSpeadUpperBound = upperSpeadUpperBound;
-            this.lowerSpeadUpperBound = lowerSpeadUpperBound;
-            this.rightSpeadUpperBound = rightSpeadUpperBound;
-            this.leftySpeadUpperBound = leftySpeadUpperBound;
-            this.upperSpeedAccelerationUpperBound = upperSpeedAccelerationUpperBound;
-            this.lowerSpeedAccelerationUpperBound = lowerSpeedAccelerationUpperBound;
-            this.rightSpeedAccelerationUpperBound = rightSpeedAccelerationUpperBound;
-            this.leftySpeedAccelerationUpperBound = leftySpeedAccelerationUpperBound;
+        this.verticalSpeed = 0;
+        this.verticalSpeedAcceleration = 0;
+        this.horizontalSpeed = 0;
+        this.horizontalSpeedAcceleration = 0;
+        this.turnAroundAnimationCount = 0;
+        this.attackAnimationCount = 0;
+        this.isTurningRight = true;
+        this.isTurningAround = false;
+
+        this.gravityCoefficient = gravityCoefficient;
+        this.characterWidth = characterWidth;
+        this.characterHeight = characterHeight;
+        this.ATTACK_ANIMATION_FRAME = ATTACK_ANIMATION_FRAME;
+        this.TURN_AROUND_FRAME = TURN_AROUND_FRAME;
+        this.x = x;
+        this.y = y;
+        this.upperSpeedAccelerationUnit = upperSpeedAccelerationUnit;
+        this.lowerSpeedAccelerationUnit = lowerSpeedAccelerationUnit;
+        this.rightSpeedAccelerationUnit = rightSpeedAccelerationUnit;
+        this.leftySpeedAccelerationUnit = leftySpeedAccelerationUnit;
+        this.upperSpeadUpperBound = upperSpeadUpperBound;
+        this.lowerSpeadUpperBound = lowerSpeadUpperBound;
+        this.rightSpeadUpperBound = rightSpeadUpperBound;
+        this.leftySpeadUpperBound = leftySpeadUpperBound;
+        this.upperSpeedAccelerationUpperBound = upperSpeedAccelerationUpperBound;
+        this.lowerSpeedAccelerationUpperBound = lowerSpeedAccelerationUpperBound;
+        this.rightSpeedAccelerationUpperBound = rightSpeedAccelerationUpperBound;
+        this.leftySpeedAccelerationUpperBound = leftySpeedAccelerationUpperBound;
     }
-    process(){
+    process() {
         this.horizontalSpeed += this.horizontalSpeedAcceleration;
-        this.x += this.horizontalSpeed;
+        if (this.horizontalSpeed <= 0) {
+            this.horizontalSpeed = this.horizontalSpeed <= -(this.leftySpeadUpperBound) ?
+                -(this.leftySpeadUpperBound) : this.horizontalSpeed;
+        }
+        else {
+            this.horizontalSpeed = this.horizontalSpeed >= this.rightSpeadUpperBound ?
+                this.rightSpeadUpperBound : this.horizontalSpeed;
+        }
     }
     // addBuff(){
     // }
