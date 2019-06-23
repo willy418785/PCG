c5548ceb4f7036b40dff3bb515bd78316b3268c6
willy418785 <spdf7785gh>
2019-06-23 15:46:45 +0800

    add interface Character

---
+++ src/Interface/Character.interface.ts
@@ -0,0 +1,32 @@
+export interface ICharacter {
+    verticalSpeed: number; // positive direction is down
+    verticalSpeedAcceleration: number;
+    horizontalSpeed: number;// positive direction is right
+    horizontalSpeedAcceleration: number;
+    turnAroundAnimationCount: number;
+    attackAnimationCount: number;
+    isTurningRight: boolean;
+    isTurningAround: boolean;
+    gravityCoefficient: number;
+    characterWidth: number;
+    characterHeight: number;
+    ATTACK_ANIMATION_FRAME: number;
+    TURN_AROUND_FRAME: number;
+    x: number;
+    y: number;
+    upperSpeedAccelerationUnit: number;
+    lowerSpeedAccelerationUnit: number;
+    rightSpeedAccelerationUnit: number;
+    leftySpeedAccelerationUnit: number;
+    upperSpeadUpperBound: number;
+    lowerSpeadUpperBound: number;
+    rightSpeadUpperBound: number;
+    leftySpeadUpperBound: number;
+    upperSpeedAccelerationUpperBound: number;
+    lowerSpeedAccelerationUpperBound: number;
+    rightSpeedAccelerationUpperBound: number;
+    leftySpeedAccelerationUpperBound: number;
+
+    process() :void;
+    intitializeContours() :void;
+}
