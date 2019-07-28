2f2ea887dd5ad8c805d98c7ed4393d20e52ac286
willy418785 <spdf7785gh>
2019-07-28 15:09:57 +0800

    set default value for constructor parameter

--- src/ContourSkeleton.ts
+++ src/ContourSkeleton.ts
@@ -3,7 +3,7 @@ import { Point } from "./Point";
 export class ContourSkeleton {
     public points: Array<Point>;
     public referencePoint: Point;
-    constructor(points: Array<Point>, referencePoint: Point) {
+    constructor(points: Array<Point>, referencePoint: Point = new Point(0, 0)) {
         this.points = points;
         this.referencePoint = referencePoint;
     }
