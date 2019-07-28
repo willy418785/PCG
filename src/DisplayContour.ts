5e37761b861d2eea198758c6640a512d8fd25241
willy418785 <spdf7785gh>
2019-07-28 15:15:36 +0800

    complete 'CollisionContours' and 'DisplayContour'

--- src/DisplayContour.ts
+++ src/DisplayContour.ts
@@ -3,18 +3,26 @@ import { ContourSkeleton } from "./ContourSkeleton";
 
 export class DisplayContour {
     public skeleton: ContourSkeleton;
-    constructor(skeleton: ContourSkeleton,) {
+    private pointFormulas: Array<(skeleton: ContourSkeleton) => Point>;
+    constructor(skeleton: ContourSkeleton, formulas : Array<(skeleton: ContourSkeleton) => Point>) {
+        this.pointFormulas = formulas;
         this.skeleton = skeleton;
     }
     getSegments(): Array<[Point, Point]> {
         let segments: Array<[Point, Point]> = new Array<[Point, Point]>();
-        for (let index = 0; index < this.points.length; index++) {
-            let lastIndex = this.points.length - 1;
+        for (let index = 0; index < this.pointFormulas.length; index++) {
+            let lastIndex = this.pointFormulas.length - 1;
             if (index != lastIndex) {
-                segments.push([this.points[index], this.points[index + 1]]);
+                segments.push([
+                    this.pointFormulas[index](this.skeleton),
+                    this.pointFormulas[index + 1](this.skeleton)
+                ]);
             }
             else {
-                segments.push([this.points[lastIndex], this.points[0]]);
+                segments.push([
+                    this.pointFormulas[lastIndex](this.skeleton),
+                    this.pointFormulas[0](this.skeleton)
+                ]);
             }
         }
         return segments;
