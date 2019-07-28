ae0b40464b30b270712c66041ae8fede6a128603
willy418785 <spdf7785gh>
2019-07-28 15:11:11 +0800

    remove 'readonly' modifier

--- src/Point.ts
+++ src/Point.ts
@@ -1,6 +1,6 @@
 export class Point {
-    public readonly x: number;
-    public readonly y: number;
+    public x: number;
+    public y: number;
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
