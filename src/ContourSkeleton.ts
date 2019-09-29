4e036a4760f7ae265d01567966574055de857e8c
willy418785 <spdf7785gh>
2019-09-29 15:31:45 +0800

    Add comment to define the order of "points".

--- src/ContourSkeleton.ts
+++ src/ContourSkeleton.ts
@@ -1,8 +1,11 @@
 import { Point } from "./Point";
 
 export class ContourSkeleton {
+    // The points is put in clockwise order.
     public points: Array<Point>;
+    
     public referencePoint: Point;
+
     constructor(points: Array<Point>, referencePoint: Point = new Point(0, 0)) {
         this.points = points;
         this.referencePoint = referencePoint;
