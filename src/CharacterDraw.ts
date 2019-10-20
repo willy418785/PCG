cb1a50ef008b06e4e29755cc4b779a1dd2d2af45
willy418785 <spdf7785gh>
2019-10-20 15:24:45 +0800

    Modify the responsbility of "CharacterDraw", to control the drawing of mutiple objects, instead of one object.

--- src/CharacterDraw.ts
+++ src/CharacterDraw.ts
@@ -1,50 +1,26 @@
 import { Character } from "./Character";
 
-export class CharacterDraw{
+export class CharacterDraw {
     character: Character;
     canvas: HTMLCanvasElement;
     ctx;
-    pLTX: number;
-    pLTY: number;
-    pRTX: number;
-    pRTY: number;
-    pRBX: number;
-    pRBY: number;
-    pLBX: number;
-    pLBY: number;
     constructor(character, canvas) {
         this.character = character;
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
-        this.pLTX = 0;
-        this.pLTY = 0;
-        this.pRTX = 0;
-        this.pRTY = 0;
-        this.pRBX = 0;
-        this.pRBY = 0;
-        this.pLBX = 0;
-        this.pLBY = 0; 
-    }
-    formBasicShape() {
-        this.pLTX = this.character.x;
-        this.pLTY = this.character.y - this.character.characterHeight;
-        this.pRTX = this.character.x + this.character.characterWidth;
-        this.pRTY = this.character.y - this.character.characterHeight;
-        this.pRBX = this.character.x + this.character.characterWidth;
-        this.pRBY = this.character.y;
-        this.pLBX = this.character.x;
-        this.pLBY = this.character.y;
     }
     draw() {
-        this.formBasicShape();
+
         this.canvas.width = this.canvas.width;
         this.ctx.strokeStyle = "black";
         this.ctx.lineWidth = 1;
-        this.ctx.lineTo(this.pLTX, this.pLTY);
-        this.ctx.lineTo(this.pRTX, this.pRTY);
-        this.ctx.lineTo(this.pRBX, this.pRBY);
-        this.ctx.lineTo(this.pLBX, this.pLBY);
-        this.ctx.lineTo(this.pLTX, this.pLTY);
+        var tmp = this.character.collisionContour.getSegments();
+
+        this.ctx.lineTo(tmp[0][0].x, tmp[0][0].y);
+        this.ctx.lineTo(tmp[1][0].x, tmp[1][0].y);
+        this.ctx.lineTo(tmp[2][0].x, tmp[2][0].y);
+        this.ctx.lineTo(tmp[3][0].x, tmp[3][0].y);
+        this.ctx.lineTo(tmp[0][0].x, tmp[0][0].y);
         this.ctx.stroke();
     }
 }
