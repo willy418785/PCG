0ff6fb8168effa7950350bbdeba899709b109930
willy418785 <spdf7785gh>
2019-10-20 15:37:23 +0800

    Add static "Point.add(Point, Point)".

--- src/Point.ts
+++ src/Point.ts
@@ -5,4 +5,10 @@ export class Point {
         this.x = x;
         this.y = y;
     }
+    static add(p1: Point, p2: Point): Point{
+        var p: Point = new Point(0, 0);
+        p.x = p1.x + p2.x;
+        p.y = p1.y + p2.y;
+        return p;
+    }
 }
