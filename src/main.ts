90c77c028ad44d1b7e1e9bd45a95d12f0c102002
willy418785 <spdf7785gh>
2019-10-20 15:38:06 +0800

    Add the usage of "ManagerForLevelAndCharacter". Add the usage of "Player".

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { CharacterDraw } from "./CharacterDraw";
 import { PlayerInput } from "./PlayerInput";
 import { EventListenerHandler } from "./EventListenerHandler";
 import { PlayerInputProcessor } from "./PlayerInputProcessor";
+import { Player } from "./Player";
+import { ManagerForLevelAndCharacter } from "./ManagerForLevelAndCharacter";
 
 var characterDraw;
 var playerInputListener;
@@ -13,6 +15,7 @@ var playerInputProcessor;
 var player;
 var FPS = 60;
 var FRAME_INTERVAL = 1000 / FPS;
+var levelManager : ManagerForLevelAndCharacter;
 function initialize(){
     var temp: HTMLElement = document.getElementById("canvas");
     var canvas: HTMLCanvasElement = <HTMLCanvasElement>temp;
@@ -38,7 +41,7 @@ function initialize(){
     var rightSpeedAccelerationUpperBound = 20;
     var leftySpeedAccelerationUpperBound = 20;   
     var ctx = canvas.getContext("2d");
-    player = new Character(
+    player = new Player(
         gravityCoefficient,
         characterWidth,
         characterHeight,
@@ -65,12 +68,15 @@ function initialize(){
     playerInputListener.AddKeypressEventListener();
     playerInputListener.AddClickEventListener();
     playerInputProcessor = new PlayerInputProcessor(player, playerInput);
+    levelManager = new ManagerForLevelAndCharacter();
+    levelManager.addCharacter(player);
+
     setTimeout(update, FRAME_INTERVAL);
 }
 function update() {
     playerInputProcessor.process();
-    player.process();
     characterDraw.draw();
+    levelManager.process();
 
     setTimeout(update, FRAME_INTERVAL);
 }
